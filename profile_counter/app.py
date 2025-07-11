ef4b6ce26c5fbd365b069f7f69fa3177082be62e
Hyejun An <jagaldol.dev@gmail.com>
2025-07-11 11:13:03 +0900

    chore: Change Port only with Docker Compose

--- profile_counter/app.py
+++ profile_counter/app.py
@@ -100,8 +100,6 @@ def root():
 
 
 if __name__ == "__main__":
-    import os
-
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0")
